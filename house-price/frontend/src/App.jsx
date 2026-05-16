075824df3b2c12c92b18198e154c8a6459b4edb9
Ohiomah Imohi <ohiomahim@gmail.com>
2026-05-16 13:01:25 -0600

    updated API_URL

--- house-price/frontend/src/App.jsx
+++ house-price/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://ml-portfolio-ryj1.onrender.com";
 
 function App() {
   const [formData, setFormData] = useState({
